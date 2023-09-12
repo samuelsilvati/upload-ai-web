0acf95be332d511b82d79ce7b50d94b4c29fc55e
Samuel Silva <samuel7silva@outlook.com>
2023-09-12 12:08:50 -0300

    fix: theme provide error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <body className={inter.className}>{children}</body>
-      </ThemeProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
