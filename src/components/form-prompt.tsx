0b0cd3c1415209b0734191c69e79a34ccc059a15
Samuel Silva <samuel7silva@outlook.com>
2023-09-18 00:12:30 -0300

    fix: parameters of the form-prompt function

--- src/components/form-prompt.tsx
+++ src/components/form-prompt.tsx
@@ -1,4 +1,7 @@
+import { Wand } from 'lucide-react'
+
 import PromptSelect from './prompt-select'
+import { Button } from './ui/button'
 import { Label } from './ui/label'
 import {
   Select,
@@ -13,6 +16,8 @@ import { Slider } from './ui/slider'
 interface FormPromptProps {
   temperature: number
   setTemperature: React.Dispatch<React.SetStateAction<number>>
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+  isLoading: boolean
 }
 
 export interface PromptSelectProps {
@@ -23,10 +28,11 @@ function FormPrompt({
   onPromptSelected,
   temperature,
   setTemperature,
+  onSubmit,
+  isLoading,
 }: PromptSelectProps & FormPromptProps) {
-  // const [temperature, setTemperature] = useState(0.5)
   return (
-    <form className="space-y-6">
+    <form className="space-y-6" onSubmit={onSubmit}>
       <div className="space-y-2">
         <Label>Prompt</Label>
         <PromptSelect onPromptSelected={onPromptSelected} />
@@ -63,6 +69,13 @@ function FormPrompt({
           Valores mais altos tendem a deixar o resultado mais criativo e com
           possíveis erros
         </span>
+
+        <Separator />
+
+        <Button type="submit" className="w-full" disabled={isLoading}>
+          Executar
+          <Wand className="ml-2 h-4 w-4" />
+        </Button>
       </div>
     </form>
   )
